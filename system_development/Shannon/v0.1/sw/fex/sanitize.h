2207a9eda6b99d5a54852537196da86478adfae2
davwamai <davwamai@gmail.com>
2024-06-18 17:55:00 -0400

    housekeeping

--- system_development/Shannon/v0.1/sw/fex/sanitize.h
+++ system_development/Shannon/v0.1/sw/fex/sanitize.h
@@ -7,25 +7,6 @@
 #define WINDOW_SIZE 125
 #define ZERO_RANGE 124
 
-/* Pulse local_extrema(DataPoint* data, int size) */
-/* { */
-/*     Pulse ext = {0.0, -1}; */
-/*     for (int i = 1; i < size - 1; i++) */
-/*     { */
-/*         if ((data[i].volt > data[i - 1].volt) && (data[i].volt > data[i + 1].volt)) */
-/*         { */
-/*                 ext.value = data[i].volt; */
-/*                 ext.index = i; */
-/*         } */
-/*         else if ((data[i].volt < data[i - 1].volt) && (data[i].volt < data[i + 1].volt)) */
-/*         { */
-/*                 ext.value = data[i].volt; */
-/*                 ext.index = i; */
-/*         } */
-/*     } */
-/*     return ext; */
-/* } */
-/**/
 Pulse local_extrema(DataPoint* data)
 {
     Pulse ext = {0.0, -1};
@@ -77,43 +58,6 @@ Pulse* remove_pulses(DataPoint* data, int size, Pulse* pulse_buffer)
     return pulse_buffer;
 }
 
-
-
-/**/
-/* Pulse* remove_pulses(DataPoint* data, int size, Pulse* pulse_buffer) */
-/* { */
-/**/
-/*     DataPoint* window_buffer = malloc(WINDOW_SIZE * sizeof(DataPoint)); */
-/*     if (window_buffer == NULL) */
-/*     { */
-/*       fprintf(stderr, "Memory allocation error.\n"); */
-/*       exit(1); */
-/*     } */
-/**/
-/*     for (int i = 0; i <= size - WINDOW_SIZE; i++) */
-/*     { */
-/*         for (int j = 0; j < WINDOW_SIZE; j++) */
-/*         { */
-/*             window_buffer[j] = data[i + j]; */
-/*         } */
-/**/
-/*         Pulse ext = local_extrema(window_buffer); */
-/**/
-/*         if (ext.index != -1 && (ext.value > THRESHOLD || ext.value < -THRESHOLD)) */
-/*         { */
-/*             pulse_buffer[i + ext.index].value = 1; */
-/*             int start = (i + ext.index - 1 < 0) ? 0 : i + ext.index - ZERO_RANGE; */
-/*             int end = (i + ext.index + 1 >= size) ? size - 1 : i + ext.index + ZERO_RANGE; */
-/*             for (int k = start; k <= end; k++) */
-/*             { */
-/*                 data[k].volt = 0; */
-/*             } */
-/*         } */
-/*     } */
-/**/
-/*     free(window_buffer); */
-/*     return pulse_buffer; */
-/* } */
 /* usage
 
     const char *input_filename = "data.txt";
