2d015900d939e6c6245f4587a4e53c25b18a97d7
Gabriel Howe <me@gabe.how>
2026-04-29 12:40:47 -0400

    add development files

--- system_development/Shannon/v0.1/eclypse/scripts/test_measurement.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-baud=115200
-stty -F /dev/ttyUSB1 $baud
-echo "rz -y" > /dev/ttyUSB1
-sz nmr_cli.py > /dev/ttyUSB1 < /dev/ttyUSB1
-echo "cnmr-driver &" > /dev/ttyUSB1
-echo "python3 nmr_cli.py --test; sz measurement.compressed_bytes" > /dev/ttyUSB1
-rz -y > /dev/ttyUSB1 < /dev/ttyUSB1
